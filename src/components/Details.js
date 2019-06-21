0ae98aaed4ad7ef3cfc2120fa268e078106757d3
Zvone <zvone.home@gmail.com>
2019-06-21 19:16:34 +0200

    fist commit adding all the components, context and styled components

---
+++ src/components/Details.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Details extends Component {
+  render() {
+    return(
+      <div>
+        <h3>This is the Details</h3>
+      </div>
+    );
+  }
+}
+
+export default Details;
---
+++ src/components/Product.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
+
+const ProductWrapper = styled.div`
+`;
+
+class Product extends Component {
+  render() {
+    const { id, title, img, price, inCart } = this.props.product;
+    return(
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div className="img-container p-5" onClick={() => console.log('click')}>
+            <Link to="/details">
+              <img scr={img} alt="product" className="card-img-top" />
+            </Link>
+          </div>  
+        </div>
+      </ProductWrapper>
+    );
+  }
+}
+
+export default Product;
